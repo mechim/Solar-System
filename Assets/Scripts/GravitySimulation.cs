239d4d5ea28d6602dabea38eafbe2ef09948b4c3
micha <michael.echim@gmail.com>
2023-07-04 10:52:59 +0300

    better visualization

--- Assets/Scripts/GravitySimulation.cs
+++ Assets/Scripts/GravitySimulation.cs
@@ -6,21 +6,36 @@ public class GravitySimulation : MonoBehaviour
 {
    CelestialBody[] bodies;
    public bool isVisualizing;
+   public int iterations;
 
    private void Awake() {
         bodies = FindObjectsOfType<CelestialBody>();
    }
 
+   private void Start() {
+        if (isVisualizing){
+            for (int j = 0; j < iterations; j ++){
+                for (int i = 0; i < bodies.Length; i ++){
+                    bodies[i].UpdateRefVelocity();
+                }
+                for (int i = 0; i < bodies.Length; i ++){
+                    bodies[i].UpdateLine();
+                }
+            }
+        }
+   }
+
     private void FixedUpdate() {
-        for (int i = 0; i < bodies.Length; i++ ){
-            bodies[i].UpdateVelocity();
-        }
-       
-        for (int i = 0; i < bodies.Length; i++){
-            if (isVisualizing == false)
+        if (!isVisualizing){
+            for (int i = 0; i < bodies.Length; i++ ){
+                bodies[i].UpdateVelocity();
+            }
+        
+            for (int i = 0; i < bodies.Length; i++){
                 bodies[i].UpdatePosition();
-            else bodies[i].UpdateLine();
+            }
         }
+        
     
    }
 }
